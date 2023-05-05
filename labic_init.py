84ff8d4d68edde48a7477d6cc7ef30dee49fe4cf
Yana Santos <59098432+Yanasants@users.noreply.github.com>
2023-05-05 15:57:50 -0300

    Versão 2.3
    adicionada opção do modelo unet original

--- labic_init.py
+++ labic_init.py
@@ -1,4 +1,5 @@
 from segmentation_models import Unet, Linknet
+from unet_original import UnetOriginal
 import json
 
 with open("config.json", "r") as params:
@@ -15,5 +16,9 @@ def labic_init(config=config):
         model = Unet(backbone_name=config["backbone_name"], encoder_weights=None,
                     input_shape=(None,None,3))
         print(f"Segmentação com modelo Unet_{backbone} inicializada com sucesso.")
+    elif config["model"] == "unet_original":
+        unet_original = UnetOriginal(input_layer_shape=(256, 256, 1))
+        model = unet_original.generate_model()
+        print(f"Segmentação com modelo Unet_original inicializada com sucesso.")
     else:
         raise ValueError("Modelo inválido! Revise o arquivo config.json")
